f120a10a7cfcb95cd4069d28e4923cc36cc4e132
JakubKolacek <102429931+JakubKolacek@users.noreply.github.com>
2022-05-23 20:50:41 +0200

    Update MSDsimple.m

--- MSDsimple.m
+++ MSDsimple.m
@@ -1,8 +1,9 @@
+% MSDsimple.m
 clear 
 clc
 
 % User-defined variables
-DataPath = "C:\COMSOL\MSD";
+DataPath = "D:\MSD";
 
 %% Script initialization
 % Creates progress bar for a better overview of the script's progress
@@ -18,48 +19,48 @@ progressbar.Message = 'Loading data';
 % Load first file for memory pre-allocation
 X = table2array(readtable(fullfile(DataPath,'x_1.csv'),'ReadVariableNames',false,'HeaderLines', 5));
 
-NSets = (length(dir(fullfile(DataPath,'*.csv')))/2); % Number of sets (Number of all csv files in the directory divided by 2)
-Nparticles = size(X,2)-1; % Since the file contains one column with time information, one is subtracted 
-Nframes = length(X(:,1)); % Reads the number of frames (rows)
+Nsets = (length(dir(fullfile(DataPath,'*.csv')))/2); 	% Number of sets (Number of all csv files in the directory divided by 2)
+Nparticles = size(X,2)-1; 				% Since the file contains one column with time information, one is subtracted 
+Nframes = length(X(:,1)); 				% Reads the number of frames (rows)
 
-time_step = (0:X(3,1)-X(2,1):X(Nframes,1))';
+tau_min = X(2,1);
+tau_max = X(Nframes,1);
+tau_step = X(3,1)-X(2,1);
+tau_plot = (tau_min:tau_step:tau_max)';
 
 % Preallocates arrays for faster computing
-Deltar = zeros(Nframes-1,Nframes-1,Nparticles);
+dr = zeros(Nframes-1,Nframes-1,Nparticles);
 MSD = zeros(Nframes-1,Nparticles);
-EN = cell(NSets,1);
+EN = cell(Nsets,1);
 
 progressbar.Value = .2;
 %% MSD Calculation
 % For each set of data
-for o=1:NSets
-    progressbar.Value = .2 + (.5*(o/NSets));
-    progressbar.Message = strcat('Processing data (Set',{' '},num2str(o),' out of  ',{' '},num2str(NSets),')');
-    
-    % Clears the previous data from arrays
-    X = zeros;
-    Y = zeros;
+for o=1:Nsets
+    % Updates the progress bar
+    progressbar.Value = .2 + (.5*(o/Nsets));
+    progressbar.Message = strcat('Processing data (Set',{' '},num2str(o),' out of  ',{' '},num2str(Nsets),')');
     
     % Reads the data from the target directory
     X = table2array(readtable(fullfile(DataPath,strcat('x_',num2str(o),'.csv')),'ReadVariableNames',false,'HeaderLines', 5));
     Y = table2array(readtable(fullfile(DataPath,strcat('y_',num2str(o),'.csv')),'ReadVariableNames',false,'HeaderLines', 5));
     
     % For each particle
-    for p=1:Nparticles
-        % For each time step
-        for n= 1:(Nframes-1)
-            m=Nframes-n;
-            % For each time difference
-            for i=1:m
-                Deltar(n,i,p)=(X(i+n,p+1)-X(i,p+1))^2+(Y(i+n,p+1)-Y(i,p+1))^2;
-            end
+    for p = 1:Nparticles
+        % For each time lag
+        for tau = 1:(Nframes-1)
+            m = Nframes - tau;
+            % For each displacement
+            for i = 1:m
+                dr(tau,i,p) = ((X(i+tau,p+1)-X(i,p+1))^2 + (Y(i+tau,p+1)-Y(i,p+1))^2);
+            end    
             % Create average for all displacements at a given time
-            MSD(1,p) = 0;
-            MSD(n+1,p) = sum(Deltar(n,:,p))/m;
+            MSD(tau,p) = sum(dr(tau,:,p))/m;
         end
     end
     % Creates ensemble average from all particles (rows)
     EN{o} = mean(MSD,2)';
+    EN_std{o} = std(MSD,0,2)';
 end
 
 %% Results - data plots
@@ -69,8 +70,12 @@ progressbar.Message = 'Plotting data';
 
 % Normal plot
 figure('Name','Normal plot');
-for z=1:NSets
-    plot(time_step,EN{z});
+for z=1:Nsets
+    %scatter(tau_plot,EN{z},'x');
+    plot(tau_plot,EN{z});
+    xlim([tau_min tau_max]);
+    xlabel('\it\tau\rm (s)');
+    ylabel('<\Delta\itr\rm^2> (m^2)');
     if z == 1
         hold on;
     end
@@ -78,8 +83,13 @@ end
 
 % Logarithmic plot (log x log y)
 figure('Name','Log plot');
-for z=1:NSets
-    loglog(time_step,EN{z});
+for z=1:Nsets
+    %scatter(tau_plot,EN{z},'x');
+    plot(tau_plot,EN{z});
+    set(gca,'xscale','log');
+    set(gca,'yscale','log');
+    xlabel('\it\tau\rm (s)');
+    ylabel('<\Delta\itr\rm^2> (m^2)');
     if z == 1
         hold on;
     end
@@ -93,5 +103,3 @@ close(fig);
 
 FileName=['workspace_',datestr(now, 'dd-mm-yyyy_HH-MM-SS')];
 save(FileName);
-
-%load 'MSD.mat' if you want to use the values 
